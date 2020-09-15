8b115722b024c4433994ad31e8df832729e160aa
Devamke <68053058+Devamke@users.noreply.github.com>
2020-09-15 22:26:44 +0300

    Update todo.py

--- todo.py
+++ todo.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,url_for,redirect,request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='/home/mrnet/flasktodoapp/templates') #templates files path
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://///home/mrnet/flasktodoapp/todo.db" #your database path
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////home/mrnet/flasktodoapp/todo.db" #your database path
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
